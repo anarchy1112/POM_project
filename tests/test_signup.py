bbe7ec8c64f22409af6add05e730f1241c227b9a
AZ <deerbeer111@gmail.com>
2022-03-16 18:02:37 +0100

    third commit

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -1,6 +1,4 @@
-import time
 
-import pages.HomePage
 from tests.BaseTest import BaseTest
 from pages.HomePage import HomePage
 
@@ -11,13 +9,11 @@ class TestSignUp(BaseTest):
     def test_dosignup(self):
         page=HomePage(self.driver)
         page.gotoReg()
-        time.sleep(1)
         page.register()
 
     def test_golog(self):
         pages=HomePage(self.driver)
         pages.gotoLogin()
-        time.sleep(1)
 
 
 
